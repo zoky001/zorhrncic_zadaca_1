bcd98b2eb9726255d81d124feb81fad3e538e8f8
Zoran Hrnčić <zorhrncic@foi.hr>
2018-10-29 20:25:03 +0100

    Init commit

---
+++ zorhrncic_zadaca_1/src/org/foi/uzdiz/zorhrncic/dz1/Zorhrncic_zadaca_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foi.uzdiz.zorhrncic.dz1;
+
+/**
+ *
+ * @author Zoran
+ */
+public class Zorhrncic_zadaca_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("parametrgggggi: " + args[0]);
+    }
+    
+}
